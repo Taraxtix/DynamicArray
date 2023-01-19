2375c130361593dc8a6ad7ba02a4eac104f8a74e
Taraxtix <antonin.brauer@yahoo.fr>
2023-01-19 01:49:05 +0100

    Initial commit

--- da.h
+++ da.h
@@ -1,10 +1,10 @@
 /**
- * @file : da.h (Main implementation of DynamicString library)
+ * @file : da.h (Main implementation of DynamicArray library)
  * @author : Taraxtix (Antonin Brauer)
  */
 
-#ifndef DS_H
-#define DS_H
+#ifndef DA_H
+#define DA_H
 
 #include <stdbool.h>
 #include <stdio.h>
@@ -12,241 +12,115 @@
 #include <string.h>
 #include <unistd.h>
 
-#ifndef DS_DEF
-    #define DS_DEF
-#endif // DS_DEF
+#ifndef DA_DEF
+    #define DA_DEF
+#endif // DA_DEF
 
 #define ASSERT(cond, msg)                                             \
-    do                                                                \
-    {                                                                 \
-        if (!(cond))                                                  \
-        {                                                             \
+    do {                                                              \
+        if (!(cond)) {                                                \
             fprintf(stderr, "[ERROR] Assertion failed: %s\n", (msg)); \
             exit(1);                                                  \
         }                                                             \
     } while (0)
 
-#define MIN(a, b)               \
-    ({                          \
-        __typeof__(a) _a = (a); \
-        __typeof__(b) _b = (b); \
-        _a < _b ? _a : _b;      \
-    })
+#define DA_GET(da, type, index) (type) * (DA_get((da), (index)))
 
 typedef struct {
     size_t size;
     size_t capacity;
-    char *data;
-} DynamicString;
-
-#define DS(str) DS_create_from_string(str)
-#define DS_NULL DS(NULL)
-
-#define DS_Fmt "%.*s"
-#define DS_Arg(sv) (int)(sv).size, (sv).data
+    void **data;
+} DynamicArray;
 
 /******************************************************************************/
 /*                           FUNCTIONS DECLARATIONS                           */
 /******************************************************************************/
 
-DS_DEF DynamicString *DS_create_from_string_parts(const char *string, size_t size);
-DS_DEF DynamicString *DS_create_from_string(const char *string);
-DS_DEF void DS_destroy(DynamicString *ds);
+DA_DEF DynamicArray *DA_create_empty();
+DA_DEF DynamicArray *DA_create_from_array(const void **array, size_t size);
+DA_DEF void DA_destroy(DynamicArray *da);
+DA_DEF DynamicArray *DA_copy(const DynamicArray *da);
+DA_DEF bool DA_equal(const DynamicArray *da1, const DynamicArray *da2);
+DA_DEF void DA_clear(DynamicArray *da);
+DA_DEF void DA_push_back(DynamicArray *da, void *element);
+DA_DEF void *DA_pop_back(DynamicArray *da);
+DA_DEF void DA_push_front(DynamicArray *da, void *element);
+DA_DEF void *DA_pop_front(DynamicArray *da);
+DA_DEF void *DA_get(const DynamicArray *da, size_t index);
+DA_DEF void DA_set(DynamicArray *da, size_t index);
+DA_DEF void DA_insert_before(DynamicArray *da, size_t index, void *element);
+DA_DEF void DA_insert_after(DynamicArray *da, size_t index, void *element);
+DA_DEF void DA_remove_at(DynamicArray *da, size_t index);
+DA_DEF void DA_remove_after(DynamicArray *da, size_t index);
+DA_DEF void DA_remove_before(DynamicArray *da, size_t index);
 
-DS_DEF void DS_clear(DynamicString *ds);
-DS_DEF bool DS_equal(const DynamicString *ds1, const DynamicString *ds2);
-DS_DEF bool DS_is_first(const DynamicString *ds, char c);
-DS_DEF void DS_append_char(DynamicString *ds, char c);
-DS_DEF void DS_append_str(DynamicString *ds, const char *str, size_t size);
-DS_DEF char DS_get_char_at(const DynamicString *ds, size_t index);
-DS_DEF void DS_set_char_at(DynamicString *ds, size_t index, char c);
-DS_DEF void DS_insert_char_at(DynamicString *ds, char c, size_t index);
-DS_DEF void DS_insert_str_at(DynamicString *ds, const char *str, size_t size, size_t index);
-DS_DEF void DS_remove_char_at(DynamicString *ds, size_t index);
-DS_DEF void DS_remove_str_at(DynamicString *ds, size_t from, size_t to);
-DS_DEF DynamicString *DS_substring(const DynamicString *ds, size_t start, size_t end);
-DS_DEF DynamicString *DS_chop_first(DynamicString *ds);
-DS_DEF DynamicString *DS_chop_last(DynamicString *ds);
-DS_DEF DynamicString *DS_chop_right(DynamicString *ds, size_t index);
-DS_DEF DynamicString *DS_chop_left(DynamicString *ds, size_t index);
-DS_DEF DynamicString *DS_chop_by_delim(DynamicString *ds, char delim);
-
-#endif // DS_H
-#ifdef DS_IMPLEMENTATION
+#endif // DA_DEF
+#ifdef DA_IMPLEMENTATION
 
 /******************************************************************************/
 /*                          FUNCTIONS IMPLEMENTATION                          */
 /******************************************************************************/
 
-DS_DEF DynamicString *DS_create_from_string_parts(const char *str, size_t size) {
-    DynamicString *ds = (DynamicString *)malloc(sizeof(DynamicString));
-    ASSERT(ds, "Cannot allocate memory for DynamicString");
-    ds->size = str ? MIN(size, strlen(str)) : 0;
-    ds->capacity = 128;
-    while (ds->capacity < ds->size)
-    {
-        if (ds->capacity == 0) ds->capacity = 128;
-        else ds->capacity *= 2;
+DA_DEF DynamicArray *DA_create_empty() {
+    DynamicArray *da = (DynamicArray *)malloc(sizeof(DynamicArray));
+    ASSERT(da, "Cannot allocate memory for dynamic array.");
+    da->size = 0;
+    da->capacity = 128;
+    da->data = malloc(sizeof(void *) * da->capacity);
+    ASSERT(da->data, "Cannot allocate memory for dynamic array.");
+    return da;
+}
+
+DA_DEF DynamicArray *DA_create_from_array(const void **array, size_t size) {
+    DynamicArray *da = DA_create_empty();
+    da->size = size;
+    while (da->capacity <= size) {
+        da->capacity *= 2;
+        da->data = realloc(da->data, sizeof(void *) * da->capacity);
     }
-    ds->data = (char *)malloc(ds->capacity);
-    ASSERT(ds->data, "Cannot allocate memory for data in the DynamicString");
-    if (str) memcpy(ds->data, str, ds->size);
-    return ds;
+    memcpy(da->data, array, sizeof(void *) * size);
+    return da;
 }
 
-DS_DEF DynamicString *DS_create_from_string(const char *string) {
-    size_t size = string ? strlen(string) : 0;
-    return DS_create_from_string_parts(string, size);
+DA_DEF void DA_destroy(DynamicArray *da) {
+    if (da) free(da->data);
+    free(da);
 }
 
-DS_DEF void DS_destroy(DynamicString *ds) {
-    if (!ds) return;
-    free(ds->data);
-    free(ds);
+DA_DEF DynamicArray *DA_copy(const DynamicArray *da) {
+    return DA_create_from_array((const void **)da->data, da->size);
 }
 
-DS_DEF void DS_clear(DynamicString *ds) {
-    ASSERT(ds, "Cannot clear a NULL DynamicString");
-    ds->size = 0;
-}
-
-DS_DEF bool DS_equal(const DynamicString *ds1, const DynamicString *ds2) {
-    ASSERT(ds1 && ds2, "Cannot compare a NULL DynamicString");
-    if (ds1->size != ds2->size) return false;
-    for (size_t i = 0; i < ds1->size; i++)
-    {
-        if (ds1->data[i] != ds2->data[i]) return false;
+DA_DEF bool DA_equal(const DynamicArray *da1, const DynamicArray *da2) {
+    if (da1->size != da2->size) return false;
+    for (size_t i = 0; i < da1->size; i++) {
+        if (da1->data[i] != da2->data[i]) return false;
     }
     return true;
 }
 
-DS_DEF bool DS_is_first(const DynamicString *ds, char c) {
-    ASSERT(ds, "Cannot access a NULL DynamicString");
-    if (ds->size == 0) return false;
-    return ds->data[0] == c;
-}
+DA_DEF void DA_clear(DynamicArray *da) {}
 
-DS_DEF void DS_append_char(DynamicString *ds, const char c) {
-    ASSERT(ds, "Cannot append to a null DynamicString");
-    if (ds->size == ds->capacity)
-    {
-        ds->capacity *= 2;
-        ds->data = (char *)realloc(ds->data, ds->capacity);
-        ASSERT(ds->data, "Cannot allocate enough memory for data in the DynamicString");
-    }
-    ds->data[ds->size++] = c;
-}
+DA_DEF void DA_push_back(DynamicArray *da, void *element) {}
 
-DS_DEF void DS_append_str(DynamicString *ds, const char *str, size_t size) {
-    ASSERT(ds, "Cannot append to a null DynamicString");
-    size = str ? MIN(size, strlen(str)) : 0;
-    while (ds->size + strlen(str) > ds->capacity)
-    {
-        ds->capacity *= 2;
-        ds->data = (char *)realloc(ds->data, ds->capacity);
-        ASSERT(ds->data, "Cannot allocate memory for data in the DynamicString");
-    }
-    memcpy(ds->data + ds->size, str, size);
-    ds->size += strlen(str);
-}
+DA_DEF void *DA_pop_back(DynamicArray *da) { return NULL; }
 
-DS_DEF char DS_get_char_at(const DynamicString *ds, size_t index) {
-    ASSERT(ds, "Cannot get char from a NULL DynamicString");
-    ASSERT(index < ds->size, "Index out of range");
-    return ds->data[index];
-}
+DA_DEF void DA_push_front(DynamicArray *da, void *element) {}
 
-DS_DEF void DS_set_char_at(DynamicString *ds, size_t index, char c) {
-    ASSERT(ds, "Cannot access a NULL DynamicString");
-    ASSERT(index < ds->size, "index out of range");
-    ds->data[index] = c;
-}
+DA_DEF void *DA_pop_front(DynamicArray *da) { return NULL; }
 
-DS_DEF void DS_insert_char_at(DynamicString *ds, char c, size_t index) {
-    ASSERT(ds, "Cannot insert into a NULL DynamicString");
-    ASSERT(index < ds->size, "Index out of range");
-    memmove(ds->data + index + 1, ds->data + index, ds->size - index);
-    ds->data[index] = c;
-    ds->size++;
-}
+DA_DEF void *DA_get(const DynamicArray *da, size_t index) { return NULL; }
 
-DS_DEF void DS_insert_str_at(DynamicString *ds, const char *str, size_t size, size_t index) {
-    ASSERT(ds, "Cannot insert into a NULL DynamicString");
-    ASSERT(index < size, "Index out of range");
-    if (!str) return;
-    size = MIN(size, strlen(str));
-    memmove(ds->data + index + size + 1, ds->data + index, ds->size - index);
-    memcpy(ds->data + index, str, size);
-    ds->size += size;
-}
+DA_DEF void DA_set(DynamicArray *da, size_t index) {}
 
-DS_DEF void DS_remove_char_at(DynamicString *ds, size_t index) {
-    ASSERT(ds, "Cannot access a NULL DynamicString");
-    ASSERT(index < ds->size, "Index out of range");
-    size_t to_move = ds->size - index - 1;
-    if (index < (ds->size - 1)) memmove(ds->data + index, ds->data + index + 1, to_move);
-    ds->size -= 1;
-}
+DA_DEF void DA_insert_before(DynamicArray *da, size_t index, void *element) {}
 
-DS_DEF void DS_remove_str_at(DynamicString *ds, size_t from, size_t to) {
-    ASSERT(ds, "Cannot access a NULL DynamicString");
-    ASSERT(from < to && to < ds->size, "Index out of range");
-    memmove(ds->data + from, ds->data + to + 1, ds->size - to - 1);
-    ds->size -= (to + 1) - from;
-}
+DA_DEF void DA_insert_after(DynamicArray *da, size_t index, void *element) {}
 
-DS_DEF DynamicString *DS_substring(const DynamicString *ds, size_t start, size_t end) {
-    ASSERT(ds, "Cannot access a NULL DynamicString");
-    ASSERT(start <= end && end < ds->size, "Invalid index range");
-    return DS_create_from_string_parts(ds->data + start, (end + 1) - start);
-}
+DA_DEF void DA_remove_at(DynamicArray *da, size_t index) {}
 
-DS_DEF DynamicString *DS_chop_first(DynamicString *ds) {
-    ASSERT(ds, "Cannot chop a NULL DynamicString");
-    if (ds->size == 0) return ds;
-    DynamicString *ret = DS_create_from_string_parts(ds->data, 1);
-    memmove(ds->data, ds->data + 1, ds->size - 1);
-    ds->size -= 1;
-    return ret;
-}
+DA_DEF void DA_remove_after(DynamicArray *da, size_t index) {}
 
-DS_DEF DynamicString *DS_chop_last(DynamicString *ds) {
-    ASSERT(ds, "Cannot chop a NULL DynamicString");
-    if (ds->size == 0) return ds;
-    DynamicString *ret = DS_create_from_string_parts(ds->data + ds->size - 1, 1);
-    ds->size -= 1;
-    return ret;
-}
+DA_DEF void DA_remove_before(DynamicArray *da, size_t index) {}
 
-DS_DEF DynamicString *DS_chop_right(DynamicString *ds, size_t index) {
-    ASSERT(ds, "Cannot chop a NULL DynamicString");
-    ASSERT(index < ds->size, "index out of range");
-    DynamicString *ret = DS_substring(ds, index, ds->size - 1);
-    DS_remove_str_at(ds, index, ds->size - 1);
-    return ret;
-}
-
-DS_DEF DynamicString *DS_chop_left(DynamicString *ds, size_t index) {
-    ASSERT(ds, "Cannot chop a NULL DynamicString");
-    ASSERT(index < ds->size, "index out of range");
-    DynamicString *ret = DS_substring(ds, 0, index);
-    DS_remove_str_at(ds, 0, index);
-    return ret;
-}
-
-DS_DEF DynamicString *DS_chop_by_delim(DynamicString *ds, char delim) {
-    ASSERT(ds, "Cannot chop a NULL DynamicString");
-    size_t index = -1;
-    for (size_t i = 0; i < ds->size; i++)
-    {
-        if (ds->data[i] == delim)
-        {
-            index = i;
-            break;
-        }
-    }
-    if (index == -1) return DS_NULL;
-    return DS_chop_left(ds, index);
-}
-
-#endif // DS_IMPLEMENTATION
+#endif // DA_IMPLEMENTATION
--- da_tests.h
+++ da_tests.h
@@ -15,24 +15,8 @@
         goto defer;        \
     } while (0)
 
-#define INT_TO_CHAR(i) (i + '0')
-
 bool test_create();
 bool test_destroy();
 bool test_clear();
-bool test_get_char_at();
-bool test_set_char_at();
-bool test_equal();
-bool test_is_first();
-bool test_append_char();
-bool test_append_string();
-bool test_remove_char_at();
-bool test_remove_string_at();
-bool test_substring();
-bool test_chop_first();
-bool test_chop_last();
-bool test_chop_right();
-bool test_chop_left();
-bool test_chop_by_delim();
 
 #endif // DA_TESTS_H
